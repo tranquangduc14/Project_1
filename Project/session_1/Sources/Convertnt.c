49066d40aa80c78408759d01cd30b3d9c26f9fb4
tranquangduc14 <105056301+tranquangduc14@users.noreply.github.com>
2022-05-07 23:03:44 +0700

    Update Convertnt.c

--- Project/session_1/Sources/Convertnt.c
+++ Project/session_1/Sources/Convertnt.c
@@ -1,15 +1,5 @@
 #include "..\Headers\Convertnt.h"
 
-// #define BYTE_TO_BINARY(byte)  \
-//         (byte & 0x80 ? '1' : '0'), \
-//         (byte & 0x40 ? '1' : '0'), \
-//         (byte & 0x20 ? '1' : '0'), \
-//         (byte & 0x10 ? '1' : '0'), \
-//         (byte & 0x08 ? '1' : '0'), \
-//         (byte & 0x04 ? '1' : '0'), \
-//         (byte & 0x02 ? '1' : '0'), \
-//         (byte & 0x01 ? '1' : '0') 
-
 void dec_to_bin(int dec){
     int count = 0;
     int bin[8];
